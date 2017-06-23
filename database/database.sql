ec1b1b26ecdc3db916fe39f62c116ea23274af48
startprogress <haobo_nice@163.com>
2017-06-23 16:37:45 +0800

    创建数据库并指定编码方式

--- database/database.sql
+++ database/database.sql
@@ -1,5 +1,6 @@
  # 创建数据库;
- CREATE DATABASE stock;
+CREATE DATABASE stock CHARACTER SET utf8 COLLATE utf8_general_ci;;
+USE stock;
  # 监管公告的数据表
 CREATE TABLE `csrc_annc_list` (
   `Annc_Key` varchar(255) COLLATE utf8_bin DEFAULT NULL,

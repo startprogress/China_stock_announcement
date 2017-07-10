058ed37da758702a8e47f2eacbaa620b3efd86d4
Haobo Zhang <haobo_nice@163.com>
2017-07-10 14:15:50 +0800

    remove useless http headers
    cookie and user-agent

--- python_scraw/cninfo_main.py
+++ python_scraw/cninfo_main.py
@@ -163,12 +163,10 @@ def parse(columntype, daterange_i, downloadpath):
             "Connection": "keep-alive",
             "Accept": "application/json, text/javascript, */*; q=0.01",
             "Origin": "http://www.cninfo.com.cn",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             "Referer": "http://www.cninfo.com.cn/cninfo-new/announcement/show",
             "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
-            "Cookie": "JSESSIONID=C462F6C346BD95BB5237F199BBB67E2F; __guid=182209569.995073424426636300.1467983882174.9705; JSESSIONID=E0CC8AB4BD2A4FE737B0E69B5B7B9E11"
+            "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6"
         }
         # request data
         data = 'stock=&searchkey=&plate=&category=&trade=&column=' + columntype + '&columnTitle=%E5%8E%86%E5%8F%B2%E5%85%AC%E5%91%8A%E6%9F%A5%E8%AF%A2&pageNum=' + \
